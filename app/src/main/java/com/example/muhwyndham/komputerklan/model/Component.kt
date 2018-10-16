5bbf7e39c8c7d1545df6926106291f5ffd9454da
Muhammad Wyndham <wyndham.haryata.p@mail.ugm.ac.id>
2018-10-16 18:53:10 +0700

    add elements to component object, convert component to room entity

--- app/src/main/java/com/example/muhwyndham/komputerklan/model/Component.kt
+++ app/src/main/java/com/example/muhwyndham/komputerklan/model/Component.kt
@@ -1,2 +1,25 @@
 package com.example.muhwyndham.komputerklan.model
 
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity
+data class Component(
+        @field:PrimaryKey
+        val id: Int,
+        val name: String,
+        val details: String,
+        val brand: Int,
+        val category: Int,
+        val subcategory: Int,
+        val brand_description: String,
+        val category_description: String,
+        val subcategory_description: String,
+        val price: Long,
+        val weight: Long,
+        val quantity: Int,
+        val stock_type: Int,
+        val link_toped: String,
+        val link_shopee: String,
+        val link_bukalapak: String
+)
